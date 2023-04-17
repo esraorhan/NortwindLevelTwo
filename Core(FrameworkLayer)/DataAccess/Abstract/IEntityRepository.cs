bdddc7bb859be06873688a3daccbebff3858e55c
esraorhan <esraorhan333@gmail.com>
2023-04-17 14:50:14 +0300

    Add project files.

---
+++ Core(FrameworkLayer)/DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_FrameworkLayer_.DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetList(Expression<Func<T,bool>> filter= null); //C# Delege Kullanımı 
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,20 @@
+﻿using Core_FrameworkLayer_.DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal :IEntityRepository<Product>
+    {
+        //veritabanın da yapmak istediğimiz içerikleri elde ederiz. 
+        //Crud işlemleri 
+        //imlementelerini concrete de olur.
+        //Repository Pattern yaklaşımı = Generic hale getirmek 
+
+        // bu kısımda generic hale getirdiğimiz sınıftan  IEntityRepository<Product> otomatik olarak product sınıfı ile çalışmıs hale gelecek. 
+    }
+}
